0a6542e4f79a06e76549c58be550d6fd50e492f2
Myfyz <1790462158@qq.com>
2018-06-26 10:13:06 +0800

    添加了一些第三方库

---
+++ MapFindHouse/MapFindHouse/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  MapFindHouse
+//
+//  Created by 范亚洲 on 2018/6/26.
+//  Copyright © 2018年 fuzhangwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor yellowColor];
+    NSLog(@"添加了一些第三方库");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MapFindHouse
-模仿链家APP地图找房
+模仿链家APP地图找房,测试一下效果
